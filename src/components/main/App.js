331e809990808c676abed0f42c0ff13a356f659a
sugardaddy123 <werowkin@gmail.com>
2022-07-03 13:52:20 +0200

    main: сделал сплиттер компонентов рабочей области

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,10 +1,15 @@
 import styles from './App.module.css'
+import WorkFrame from "./WorkFrame";
 
 function App() {
-
     return (
         <div className={styles.App}>
-            App
+            <WorkFrame
+                firstFrame={<div style={{backgroundColor: '#add7f6'}}>Hello world</div>}
+                secondFrame={<div style={{backgroundColor: '#87bfff'}}>Hello you</div>}
+                thirdFrame={<div style={{backgroundColor: '#3f8efc'}}>Hello friends</div>}
+                fourthFrame={<div style={{backgroundColor: '#2667ff'}}>Hello everyone</div>}
+            />
         </div>
     );
 }
---
+++ src/components/main/WorkFrame.js
@@ -0,0 +1,63 @@
+import styles from './WorkFrame.module.css';
+import Splitter from "m-react-splitters";
+
+
+
+function WorkFrame(props) {
+    return (
+        <div className={styles.workFrame}>
+            <Splitter
+                position="vertical"
+                primaryPaneWidth="240px"
+                primaryPaneMaxWidth="100%"
+                primaryPaneMinWidth={0}
+                dispatchResize={true}
+                postPoned={false}
+            >
+                {/* LEFT WORK-AREA */}
+                <Splitter
+                    position="horizontal"
+                    primaryPaneHeight={340}
+                    primaryPaneMaxHeight="100%"
+                    primaryPaneMinHeight={0}
+                >
+                    <div
+                        className={styles.frame}
+                        style={{backgroundColor: '#add7f6'}}
+                    >
+                        {props.firstFrame}
+                    </div>
+                    <div
+                        className={styles.frame}
+                        style={{backgroundColor: '#3f8efc'}}
+                    >
+                        {props.thirdFrame}
+                    </div>
+                </Splitter>
+
+                {/* RIGHT WORK-AREA */}
+                <Splitter
+                    position="horizontal"
+                    primaryPaneHeight={450}
+                    primaryPaneMaxHeight="100%"
+                    primaryPaneMinHeight={0}
+                >
+                    <div
+                        className={styles.frame}
+                        style={{backgroundColor: '#87bfff'}}
+                    >
+                        {props.secondFrame}
+                    </div>
+                    <div
+                        className={styles.frame}
+                        style={{backgroundColor: '#2667ff'}}
+                    >
+                        {props.fourthFrame}
+                    </div>
+                </Splitter>
+            </Splitter>
+        </div>
+    )
+}
+
+export default WorkFrame;
