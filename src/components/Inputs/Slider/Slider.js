8917b5df7fd273f404d5f21353af040ba29293ef
MelnikovSavva <msavva@bk.ru>
2022-07-10 19:19:39 +0200

    Реализовал изменение положения слайдера через кнопки

--- src/components/Inputs/Slider/Slider.js
+++ src/components/Inputs/Slider/Slider.js
@@ -1,31 +1,31 @@
 import ReactSlider from "react-slider";
 import './Slider.css';
 import {SliderButton} from "./SliderButton";
-import { useState } from "react";
+import {useStatem, useEffect, useState} from "react";
 
 export function Slider({id}){
-	// let [currentValue] = useState(0);
-	// function setCurrentValue(value){
-	// 	currentValue = value;
-	// }
+	const [currentValue, setCurrentValue] = useState(0);
+
 	return (
 		<div className="SliderWrap">
 			<SliderButton
 				id = {id}
 				title={'-'}
-				// currentValue={currentValue}
-				// onClick={() => setCurrentValue(++currentValue)}
+				currentValue={currentValue}
+				onClick={() => setCurrentValue(currentValue - 1)}
 			/>
 			<ReactSlider
 				className="Slider"
 				trackClassName="SliderTrack"
 				thumbClassName="SliderThumb"
-				// value={currentValue}
-				// onChange={(value) => setCurrentValue(value)}
+				value={currentValue}
+				onChange={(value) => setCurrentValue(value)}
 			/>
 			<SliderButton
 				id = {id}
 				title={'+'}
+				currentValue={currentValue}
+				onClick={() => setCurrentValue(currentValue + 1)}
 			/>
 		</div>
 
--- src/components/Inputs/Slider/SliderButton.js
+++ src/components/Inputs/Slider/SliderButton.js
@@ -3,8 +3,8 @@ import styles from './SliderButton.module.css'
 export function SliderButton({id, title, currentValue,onClick}){
 
 	return(
-		<button className={styles.SliderButton} onClick={currentValue++}>
-			<span>
+		<button className={styles.SliderButton} onClick={onClick}>
+			<span className={styles.SliderButtonText}>
 				{title}
 			</span>
 		</button>
